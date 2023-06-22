eab0c3063c0161e3eb7a234f3f0ea0867e0eca87
CHRISHA1122 <chrisha1122@outlook.com>
2023-06-22 14:41:32 +0000

    Added product view, url and template

--- products/views.py
+++ products/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
 from django.db.models.functions import Lower
+
 from .models import Product, Category, CustomizableProduct
 from .forms import CustomizableProductForm
+from .forms import ProductForm
 
 # Create views
 
@@ -75,6 +77,17 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+
+
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
 
 
 def customize_product(request, product_id):
