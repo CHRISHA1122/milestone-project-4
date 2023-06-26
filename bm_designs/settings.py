79fb06f4de128e6e3cb68f2534e3ab3dd14e9640
CHRISHA1122 <chrisha1122@outlook.com>
2023-06-26 23:13:01 +0000

    Removed secret key

--- bm_designs/settings.py
+++ bm_designs/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rl^nn8xg+5t0wic1j!*7eevo9v3smf5=7ln(ks6(34o=^fjbq='
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['milestone-project-4-bmdesigns.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['milestone-project-4-bmdesigns.herokuapp.com', 'localhost', 'milestone-project-4-bmdesigns-bfbe9959c9ae.herokuapp.com']
 
 ACCOUNT_EMAIL_VERIFICATION = 'none'
 
