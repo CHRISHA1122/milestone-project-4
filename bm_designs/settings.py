cacc7e4ab3e4bc77fc9fe718c06d8baa4807731e
CHRISHA1122 <chrisha1122@outlook.com>
2023-06-27 11:09:16 +0000

    Added AWS static

--- bm_designs/settings.py
+++ bm_designs/settings.py
@@ -186,7 +186,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Config
-    AWS_STORAGE_BUCKET_NAME = 'bmdesigns-project'
+    AWS_STORAGE_BUCKET_NAME = 'milestone-project-4-bmdesigns'
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
